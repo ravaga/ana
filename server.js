e84d9a0cf201fd30f2a17373d36ad53ca39b2672
Rafa <rafael.valdespino@g-global.com.mx>
2016-12-19 10:32:00 -0800

    code cleanup

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require('express')
 var app = express()
+var phpServer = require('node-php-server')
+
+
+phpServer.createServer({
+    port: 8000,
+    hostname: '127.0.0.1',
+    base: '.',
+    keepalive: false,
+    open: false,
+    bin: 'php',
+    router: __dirname + '/server.php'
+});
+
+
 
 app.use('/', express.static(__dirname + '/'));
 
@@ -7,3 +21,4 @@ app.listen(3000, function(){
 	console.log('Listening on port 3000')
 
 });
+
